cc544f796bf3c660cd425b84e4046a11efc89d0a
69410126 <kaiden12345do@gmail.com>
2022-10-28 11:57:41 -0700

    alter

--- model_blackjack.py
+++ model_blackjack.py
@@ -1,84 +1,70 @@
 import random
 
-trfa_data = []
-trfa_list = [
-    "In the animation film “Finding Nemo,” the main protagonist is a pufferfish.",
-    "Is Mount Kilimanjaro the world’s tallest peak?",
-    "Spaghetto is the singular form of the word spaghetti.",
-    "Pinocchio was Walt Disney’s first animated feature film in full color.",
-    "Venezuela is home to the world’s highest waterfall.",
-    "Coffee is a berry-based beverage.",
-    "The capital of Australia is Sydney.",
-    "The longest river in the world is the Amazon River.",
-    "Polar bears can only live in the Arctic region, not in the Antarctic.",
-    "The United Kingdom is almost the same size as France.",
-    "Walt Disney has the record for most Academy Awards.",
-    "By lying a frog on its back and softly caressing its tummy, it is possible to hypnotize it.",
-    "The moon is wider than Australia.",
-    "Daily, your nose and sinuses create almost one liter of mucus.",
-    "Seahorses have stomachs, which aid in the digestion of food.",
-    "The first Disney princess was Cinderella.",
-    "In Scotland, the unicorn is their national animal.",
-    'Zeus is referred to as the king of the Greek gods and goddesses.'
+rate_data = []
+rate_list = [
+    "Was Pong fun?",
+    "Was Blackjack fun?",
+    "Was Snake fun?",
+    'Was the Jokebox fun?'
 ]
 
 # Initialize True False
-def initTrfa():
+def initRate():
     # setup jokes into a dictionary with id, trfa, true, false
     item_id = 0
-    for item in trfa_list:
-        trfa_data.append({"id": item_id, "trfa": item, "true": 0, "false": 0})
+    for item in rate_list:
+        rate_data.append({"id": item_id, "rate": item, "yes": 0, "no": 0})
         item_id += 1
     # prime some true responses
     for i in range(10):
-        id = getRandomTrfa()['id']
-        addTrfaTrue(id)
+        id = getRandomRate()['id']
+        addRateYes(id)
     # prime some false responses
     for i in range(5):
-        id = getRandomTrfa()['id']
-        addTrfaFalse(id)
+        id = getRandomRate()['id']
+        addRateNo(id)
         
 # Return all jokes from jokes_data
-def getTrfas():
-    return(trfa_data)
+def getRates():
+    return(rate_data)
 
 # Joke getter
-def getTrfa(id):
-    return(trfa_data[id])
+def getRate(id):
+    return(rate_data[id])
 
 # Return random joke from jokes_data
-def getRandomTrfa():
-    return(random.choice(trfa_data))
+def getRandomRate():
+    return(random.choice(rate_data))
 
 # Liked joke
-def favoriteTrfa():
+def favoriteRate():
     best = 0
     bestID = -1
-    for trfa in getTrfas():
-        if trfa['true'] > best:
-            best = trfa['true']
-            bestID = trfa['id']
-    return trfa_data[bestID]
+    for rate in getRates():
+        if rate['yes'] > best:
+            best = rate['yes']
+            bestID = rate['id']
+    return rate_data[bestID]
     
 # Jeered joke
-def worstTrfa():
+def worstRate():
     worst = 0
     worstID = -1
-    for trfa in getTrfas():
-        if trfa['false'] > worst:
-            worst = trfa['boohoo']
-            worstID = trfa['id']
-    return trfa_data[worstID]
+    for rate in getRates():
+        if rate['false'] > worst:
+            worst = rate['no']
+            worstID = rate['id']
+    return rate_data[worstID]
 
 # Add to haha for requested id
-def addTrfaTrue(id):
-    trfa_data[id]['true'] = trfa_data[id]['true'] + 1
-    return trfa_data[id]['true']
+def addRateYes(id):
+    rate_data[id]['yes'] = rate_data[id]['yes'] + 1
+    return rate_data[id]['yes']
 
 # Add to boohoo for requested id
-def addTrfaFalse(id):
-    trfa_data[id]['false'] = trfa_data[id]['false'] + 1
-    return trfa_data[id]['false']
+def addRateNo(id):
+    rate_data[id]['no'] = rate_data[id]['no'] + 1
+    return rate_data[id]['false']
 
 # Pretty Print joke
 def printTrfa(trfa):
